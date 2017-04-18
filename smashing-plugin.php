ded21f2899badd6318fedefe03fe83fdcd0de112
ullakalaim8 <kaleemon.mohammed@gmail.com>
2017-04-18 12:55:23 +0530

    Update smashing-plugin.php

--- smashing-plugin.php
+++ smashing-plugin.php
@@ -2,7 +2,7 @@
 /*
 	Plugin Name: Smashing Plugin
 	Description: This is for updating your Wordpress plugin.
-	Version: 1.0.1
+	Version: 1.0.2
 	Author: Kalim
 */
 if( ! class_exists( 'Smashing_Updater' ) ){
